599de1680fad6d4ab3b7d1c603edf4ca35ab5430
jaqmol <ronny.reichmann@gmail.com>
2019-06-13 20:19:28 +0200

    refinements

--- example/static_file_server/lib/lookup_media_type.js
+++ example/static_file_server/lib/lookup_media_type.js
@@ -3,36 +3,40 @@ const readline = require('readline');
 
 readMediaTypeForExtension((err, mediaTypeForExt) => {
   if (err) {
-    console.error(err);
+    logError(err);
     process.exit(1);
   } else {
     listenForInput(mediaTypeForExt);
   }
 })
 
+function logError(id, err) {
+  const errActn = {
+    axmsg: 1,
+    id,
+    role: 'error',
+    data: {
+      source: "lookup_mime_type",
+      code: 10001,
+      message: err.message,
+      fileName: err.fileName,
+      lineNumber: err.lineNumber,
+    },
+  };
+  const errStr = JSON.stringify(errActn) + '\n';
+  process.std.write(errStr, 'utf8');
+}
+
 function listenForInput(mediaTypeForExt) {
   const rl = readline.createInterface({
-    input: process.stdin, // fs.createReadStream('sample.txt'),
+    input: process.stdin,
     crlfDelay: Infinity
   });
 
   rl.on('line', (line) => {
-    // expecting AX action message:
-    // {
-    //   axmsg: int,
-    //   id: int,
-    //   seq: int,
-    //   role: string,
-    //   cmd: string,
-    //   data: {
-    //     method: string,
-    //     url: string,
-    //     header: object,
-    //     bodyB64: string,
-    //   },
-    // }
-    // example:
+    // input example:
     // {"axmsg":1,"id":21,"role":"http-request","data":{"method":"GET","url":"http://localhost:3000/index.html"}}
+    // {"axmsg":1,"id":21,"role":"http-request","data":{"method":"GET","url":"http://localhost:3000/images/logo.png"}}
     const inActn = JSON.parse(line);
     if (inActn.axmsg === 1 && inActn.role === 'http-request' && inActn.data.method === 'GET') {
       let extension = filenameExtension(inActn.data.url);
