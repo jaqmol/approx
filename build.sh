820b233282b71fc3fb93719df5664b02ed1a6106
jaqmol <ronny.reichmann@gmail.com>
2019-06-25 21:40:41 +0200

    refactoring build

--- build.sh
+++ build.sh
@@ -2,25 +2,27 @@
 
 # bin=${PWD##*/}
 
-echo Please make sure the sources of the following processors are situated in the parent directory:
-echo   - approx_http_server
-echo   - approx_fork
-echo   - approx_merge
-echo   - approx_check
-
+echo Building approx itself...
 rm -rf bin
 mkdir -p bin/lib
-
 go build -o bin/approx
+echo ...done.
 
+echo Building assistive processors in parent directory...
+
+echo - approx_http_server
 cd ../approx_http_server
 go build -o ../approx/bin/lib/http_server
 
+echo - approx_fork
 cd ../approx_fork
 go build -o ../approx/bin/lib/fork
 
+echo - approx_merge
 cd ../approx_merge
 go build -o ../approx/bin/lib/merge
 
+echo - approx_check
 cd ../approx_check
 go build -o ../approx/bin/lib/check
+echo ...done.
